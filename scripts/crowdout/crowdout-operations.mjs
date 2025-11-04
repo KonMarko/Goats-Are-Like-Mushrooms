119052c520aa09506c83c50ce5277928feac50e6
KonMarko <konrad.markowski@bridebook.co.uk>
2025-11-04 15:15:09 +0100

    add extra space before emoji

--- scripts/crowdout/crowdout-operations.mjs
+++ scripts/crowdout/crowdout-operations.mjs
@@ -139,7 +139,7 @@ const createTranslationMessage = (links) => {
   const messageBase= `Hello :wave:, can I have translations for these please?
 ${Object.entries(links).map(([lang, urls]) => {
   const emoji = `:${lang.split('-')[0]}:`;
-  return `${emoji}:\n${urls.map(url => {
+  return `\n${emoji}:\n${urls.map(url => {
     const {link, namespace, language} = url
     return `• <${link}| Crowdout | ${namespace} | ${language}>`;
   }).join('\n')}`;
